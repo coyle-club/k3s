443aa1580f4ebb54a35631285f323b25531e853c
Tom Petr <trpetr@gmail.com>
2022-03-06 10:14:40 -0500

    more work

--- setup.sh
+++ setup.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 
-k3s-uninstall.sh
+if [[ -f /usr/local/bin/k3s-uninstall.sh ]]; then
+	k3s-uninstall.sh
+fi
 
-curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true sh -
+if [[ ! -d /opt/cni/bin ]]; then
+	sudo mkdir -p /opt/cni/bin
+	sudo curl "https://github.com/containernetworking/plugins/releases/download/v1.1.0/cni-plugins-linux-arm-v1.1.0.tgz" | tar xvzf -C /opt/cni/bin
+fi
 
-sudo mkdir /etc/rancher/k3s
+curl -sfL https://get.k3s.io | INSTALL_K3S_SKIP_START=true sh - $@
+
+sudo mkdir -p /etc/rancher/k3s
 
 sudo cp config.yaml /etc/rancher/k3s/
 sudo cp 10-coylenet.conflist /etc/cni/net.d/
 
 sudo service k3s start
 
-kubectl apply -f dns.yaml
+kubectl apply -f subnets.yaml
 
-kubectl apply -f nginx.yaml
 
-kubectl apply -f expose.yaml
+#kubectl apply -f dns.yaml
+#kubectl apply -f expose.yaml
 
-kubectl create namespace cert-manager
-kubectl apply -f cert-manager-arm.yaml
+#kubectl create namespace cert-manager
+
+#kubectl apply -f cert-manager.yaml
+
+#if [[ -f /etc/cloudflare/cloudflare-api-token ]]; then
+#	kubectl create secret generic cloudflare --from-file=cloudflare-api-token=/etc/cloudflare/cloudflare-api-token
+#fi
+#kubectl apply -f letsencrypt.yaml
